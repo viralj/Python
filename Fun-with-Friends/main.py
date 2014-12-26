a46bdce98d7fdfafd34da18a00ca8036964794cd
Viral Joshi <viralj@users.noreply.github.com>
2014-12-26 04:59:06 -0600

    Create main.py

---
+++ Fun-with-Friends/main.py
@@ -0,0 +1,14 @@
+"""
+Fun with Friends
+/*
+ *	Author Name: Viral Joshi
+ *
+ *	Join me on 	 github		: /viralj
+ *				 facebook	: /viral4ever
+ *				 google+	: /+ViralJoshi
+ *				 twitter	: /viralhj
+ *				 linkedin	: /in/viralj
+ *
+ *
+ */
+"""
