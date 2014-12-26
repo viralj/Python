0c08465140f2edd9fafb18cc8cb1650174a136a5
Viral Joshi <viralj@users.noreply.github.com>
2014-12-26 05:00:35 -0600

    Create main.py

--- Fun-with-Friends/main.py
+++ Fun-with-Friends/main.py
@@ -11,4 +11,17 @@ Fun with Friends
  *
  *
  */
+ 
+ required python version :    Python 3.0+
 """
+import os
+
+clear = lambda: os.system('cls')    # A lambda function to clear screen
+
+total_question = int(input("How many funny lines you want to set up for your friends? "))
+text = []
+
+while int(total_question) > 10:
+    print("Not more than 10 because it is easy to remember what you will enter.")
+    print("")
+    total_question = int(input("How many funny lines you want to set up for your friends? "))
