3d64ed7e8dfd5e3eada51d3241d4953e5135cb7c
Viral Joshi <viralj@users.noreply.github.com>
2014-12-27 21:47:04 -0600

    Create main.py

---
+++ Encryption/main.py
@@ -0,0 +1,22 @@
+"""
+Encryption
+/*
+ *	Author Name: Viral Joshi
+ *
+ *	Join me on 	 github		: /viralj
+ *				 facebook	: /viral4ever
+ *				 google+	: /+ViralJoshi
+ *				 twitter	: /viralhj
+ *				 linkedin	: /in/viralj
+ *
+ *
+ */
+
+ required python version :    Python 3.0+
+"""
+
+from Encryption import Encryption   # Importing custom Encryption class
+from Decryption import Decryption   # Importing custom Decryption class
+import time     # Importing for time delay
+
+a = str(input("Please enter ENCRYPTION or DECRYPTION : ")).lower()  # Need user input to know next action
