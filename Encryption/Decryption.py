98a114c10f84c56702cca33b80680392a9b79498
Viral Joshi <viralj@users.noreply.github.com>
2014-12-27 21:49:03 -0600

    Create Decryption.py

---
+++ Encryption/Decryption.py
@@ -0,0 +1,20 @@
+"""
+Custom Decryption class
+
+/*
+ *	Author Name: Viral Joshi
+ *
+ *	Join me on 	 github		: /viralj
+ *				 facebook	: /viral4ever
+ *				 google+	: /+ViralJoshi
+ *				 twitter	: /viralhj
+ *				 linkedin	: /in/viralj
+ *
+ *
+ */
+
+ required python version :    Python 3.0+
+
+"""
+
+class Decryption:
