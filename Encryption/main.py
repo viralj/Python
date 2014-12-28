7b79e8bab8e03aabeb3125ce4232bed664c4bce9
Viral Joshi <viralj@users.noreply.github.com>
2014-12-27 21:47:28 -0600

    Create main.py

--- Encryption/main.py
+++ Encryption/main.py
@@ -20,3 +20,17 @@ from Decryption import Decryption   # Importing custom Decryption class
 import time     # Importing for time delay
 
 a = str(input("Please enter ENCRYPTION or DECRYPTION : ")).lower()  # Need user input to know next action
+
+"""
+Action for Encryption or Decryption
+starts from here.
+"""
+if a == "encryption":
+    b = str(input("Please enter a string : "))
+    Encryption.encryption(b)
+elif a == "decryption":
+    Decryption.decryption()
+else:
+    print("Wrong answer! Good bye...")
+    time.sleep(1)
+    exit()
